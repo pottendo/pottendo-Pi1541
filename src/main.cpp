23bbe6ea7d0b082f1480a75c9cb6fed63a5a912f
pottendo <pottendo@gmx.net>
2025-04-18 23:28:35 +0200

    Support Zak German original

--- src/main.cpp
+++ src/main.cpp
@@ -916,7 +916,8 @@ EXIT_TYPE __not_in_flash_func(Emulate1541) (FileBrowser* fileBrowser)
 	// 0x2a7f4b77 = zak_mckracken_boot[activision_1988](manual)(!).g64
 	// 0x97732c3e = maniac_mansion_s1[activision_1987](!).g64
 	// 0x63f809d2 = 4x4_offroad_racing_s1[epyx_1988](ntsc)(!).g64
-	if (hash == 0x42c02586 || hash == 0x18651422 || hash == 0x2a7f4b77 || hash == 0x97732c3e || hash == 0x63f809d2)
+	DEBUG_LOG("%s: hash = %x", __FUNCTION__, hash);
+	if (hash == 0x42c02586 || hash == 0x18651422 || hash == 0x2a7f4b77 || hash == 0x97732c3e || hash == 0x63f809d2 || hash == 0x778fecda)
 	{
 		refreshOutsAfterCPUStep = false;
 	}
